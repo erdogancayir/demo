e5fd4f9b1a4134dcf715c748b7f3184cdbccfff9
Erdogan Cayir <ecayir@k2m01s04.42kocaeli.com.tr>
2023-03-04 18:31:32 +0300

    firt push

---
+++ fronted/src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home-page">
+    <h1>Hoş Geldiniz!</h1>
+    <div class="button-group">
+      <button @click="registerFunc()" class="register-button">Kayıt Ol</button>
+      <button class="login-button">Giriş Yap</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  methods: {
+    registerFunc () {
+      this.$router.push({ path: '/register' })
+    }
+  }
+
+})
+</script>
+
+<style>
+.home-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.button-group {
+  display: flex;
+  flex-direction: row;
+  margin-top: 20px;
+}
+
+.register-button {
+  margin-right: 10px;
+}
+
+.login-button {
+  margin-left: 10px;
+}
+</style>
